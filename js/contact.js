e8b2fcbaa05ca2ebac3b3775e6fc1f1adac2fd5f
Daniel Awolowo <awolowodaniel@yahoo.ie>
2022-12-09 14:19:26 +0000

    first commit

---
+++ js/contact.js
@@ -0,0 +1,54 @@
+//Get data
+const nameInput = document.querySelector("#name");
+const email = document.querySelector("#email");
+const message = document.querySelector("#message");
+const success = document.querySelector("#success");
+const errorNodes = document.querySelectorAll(".error");
+
+
+//Validate data
+function validateForm(){
+
+  clearMessages();
+  let errorFlag = false;
+
+  if(nameInput.value.length < 1){
+    errorNodes[0].innerText = "Name can't be blank";
+    nameInput.classList.add("error-border");
+    errorFlag = true;
+  }
+
+  if(!emailIsValid(email.value)){
+    errorNodes[1].innerText = "Invalid email address";
+    email.classList.add("error-border");
+    errorFlag = true;
+  }
+
+  if(message.value.length < 1){
+    errorNodes[2].innerText = "Please enter a response";
+    message.classList.add("error-border");
+    errorFlag = true;
+  }
+
+  if(!errorFlag){
+    success.innerText = "Success!";
+  }
+}
+
+//Clear error / success messages
+function clearMessages(){
+    for(let i = 0; i < errorNodes.length; i++){
+      errorNodes[i].innerText = "";
+    }
+    success.innerText = "";
+    nameInput.classList.remove("error-border");
+    email.classList.remove("error-border");
+    message.classList.remove("error-border");
+
+}
+
+//Check if email is valid
+function emailIsValid(email){
+  let pattern = /\S+@\S+\.\S+/;
+  return pattern.test(email);
+}
